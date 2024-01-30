b1565fbffad1c622750792f55e4b127ea25af67c
Aashir <aashirnawaz5@gmail.com>
2024-01-30 15:42:55 +0500

    index.html sent

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'purple':'#5D2F61',
+        'grey':'#E6E6E6',
+        'blue':'#3F3F7A',
+        'red':'#774040'
+      }
+    },
+  },
+  plugins: [],
+}
+
